345be446d398bc0f339f419cd9a2a3d9800c5517
eatmeat <lordxxl@bk.ru>
2024-04-20 05:00:53 +0800

    first

---
+++ app/classes/Protocol.py
@@ -0,0 +1,340 @@
+import logging
+import socket
+import time
+import threading
+import random
+import logging
+import json
+import re
+import hashlib
+
+missed_messages = (set())                               # Storage for hashes of required but missed messages (to request them later)
+_alreadyused = set()
+
+pool = []                                               # Pool for commands to any session (like RQD to initiate data update)
+data = {}
+sessions = []
+
+def getSessions():
+    global sessions
+    return sessions
+
+def randomport():
+    global _alreadyused
+    p = random.randint(16000, 65535)
+    while p in _alreadyused:
+        p = random.randint(16000, 65535)
+    _alreadyused.update({p})
+    return p
+
+def aegis(f):
+    def wr():
+        while 1:
+            try:
+                f()
+                break
+            except Exception as e:
+                logging.critical(e)
+    return wr
+
+def dat_to_bytes(diction: dict) -> bytes:
+    return json.dumps(diction).encode("cp866")
+
+def bytes_to_dat(byte: bytes) -> dict:
+    return json.loads(byte.decode("cp866"))
+
+def get_tree():
+    global data
+    data = data.copy()
+    data.update({"00000000": "Root"})
+    data.update({"ffffffff": "Lost"})
+    dct = {}
+
+    for key, message in data.items():
+        if key + ": " + message not in dct:
+            dct.update({key + ": " + message: []})
+
+    for key, message in data.items():
+        rep = [i[1:] for i in re.findall("@+[a-z0-9]{8}", message)]
+
+        if len(rep) > 0:
+            rep = rep[0]
+        else:
+            rep = "00000000"
+
+        if rep not in data:
+            rep = "ffffffff"
+        if rep + ": " + data[rep] in dct:
+            dct[rep + ": " + data[rep]].append(key + ": " + message)
+        else:
+            pass
+            # dct.update({rep+": "+data[rep]:[key+": "+message]})
+    # print(dct)
+    dct["00000000: Root"].remove("00000000: Root")
+    for k, v in list(dct.items()).copy():
+        if v == []:
+            dct.pop(k)
+    return dct
+
+def checksum(b):
+    return hashlib.blake2s(b, digest_size=4).hexdigest()
+
+def data_add(item):
+    data.update({checksum(item.encode("cp866")): item})
+
+def data_dump():
+    with open("chat-savefile.json", "w") as f:
+        json.dump(data, f)
+
+def data_load():
+    global data
+    try:
+        with open("chat-savefile.json", "r") as f:
+            data = json.load(f)
+    except:
+        logging.error(f"No save file exists! New one created")
+        with open(f"chat-savefile.json", "w") as f:
+            json.dump({}, f)
+
+class Session:
+    def __init__(self, port):
+        # self.prefix="IDL"
+        self.immortal = False
+        self.local_port = port
+        for i in range(10):
+            
+            host="stun.ekiga.net"
+            logging.debug(f"STUN request via {host}")
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.bind(("0.0.0.0", self.local_port))
+            sock.setblocking(0)
+            server = socket.gethostbyname(host)
+            work = True
+            while work:
+                sock.sendto(b"\x00\x01\x00\x00!\x12\xa4B\xd6\x85y\xb8\x11\x030\x06xi\xdfB", (server, 3478), )
+                for i in range(20):
+                    try:
+                        ans, addr = sock.recvfrom(2048)
+                        work = False
+                        break
+                    except:
+                        time.sleep(0.01)
+            sock.close()
+        
+        self.public_ip = socket.inet_ntoa(ans[28:32])
+        self.public_port = int.from_bytes(ans[26:28], byteorder="big")
+        self.socket = None
+        self.client = None
+        self.thread = None
+        logging.info(f'"{self.public_ip}",{self.public_port}')
+    
+    def getPublic_ip(self):
+       return self.public_ip 
+
+    def make_connection(self, ip, port, timeout=10):
+        logging.debug(f"Start waiting for handshake with {ip}:{port}")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind(("0.0.0.0", self.local_port))
+        sock.setblocking(0)
+        while True:
+            sock.sendto(b"Con. Request!", (ip, port))
+            time.sleep(2)
+            try:
+                ans, addr = sock.recvfrom(9999)
+                sock.sendto(b"Con. Request!", (ip, port))
+                sock.close()
+                sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                sock.bind(("0.0.0.0", self.local_port))
+                sock.setblocking(0)
+                self.client = (ip, port)
+                self.socket = sock
+                logging.debug(f"Hole with {self.client} punched!")
+                break
+            except Exception as e:
+                assert timeout > 0
+                timeout -= 1
+                logging.debug(f"No handshake with {ip}:{port} yet...")
+
+    def backlife_cycle(self, freq=1):
+        global sessions
+        if self.immortal:
+            logging.warning(f"{self.client} session beacame immortal")
+            self.life_cycle = aegis(self.life_cycle)
+        th = threading.Thread(target=self.life_cycle, args=(freq,))
+        th.start()
+        self.thread = th
+        logging.warning(f"Session with {self.client} stabilized!")
+        # sessions.append(self)
+
+    def life_cycle(self, freq=1):
+        global data
+        global sessions
+        global pool
+        c = 0
+        while 1:
+            if len(pool):
+                pref = pool.pop(0)
+            else:
+                pref = b"KPL"
+
+            self.socket.sendto(pref, self.client)  # Keep-alive
+            time.sleep(max(random.gauss(1 / freq, 3), 0))
+
+            while True:
+                try:
+                    ans, reply_addr = self.socket.recvfrom(9999)
+                    logging.debug(
+                        f"{self.client[0]}: Recieved {ans[:3].decode('cp866')} from {reply_addr}: {ans}"
+                    )
+                except:
+                    break
+
+                if ans[:3] == b"KPL":
+                    c += 1
+                    if c % 10 == 0:
+                        logging.debug(
+                            f"{self.client[0]}: Requesting Datagram     {c//10}"
+                        )
+                        self.socket.sendto(b"RQD", self.client)
+                        c += 1
+                    elif c % 33 == 0:
+                        logging.debug(
+                            f"{self.client[0]}: Requesting Session List {c//33}"
+                        )
+                        self.socket.sendto(b"RQS", self.client)
+                        c += 1
+
+                # ----------------------------------------Hashes (keys) sync (disabled now)----------------------------------------
+                elif ans[:3] == b"RQH":
+                    logging.debug(f"{self.client[0]}: Sending hashes")
+                    self.socket.sendto(
+                        b"HAS" + dat_to_bytes(list(data.keys())), self.client
+                    )
+
+                elif ans[:3] == b"HAS":
+                    missed_messages.update(
+                        set(bytes_to_dat(ans[3:])) - set(data.keys())
+                    )
+
+                # ----------------------------------------Data sync----------------------------------------
+                elif ans[:3] == b"RQD":
+                    if ans[3:] != b"":
+                        logging.debug(f"{self.client[0]}: Sending specific datagram")
+                        n = {}
+                        r = set(bytes_to_dat(ans[3:]))
+                        for i in r:
+                            if i in data:
+                                n.update({i: data[i]})
+                        logging.debug(f"{self.client[0]}: Sending {n}")
+                        self.socket.sendto(b"DAT" + dat_to_bytes(n), self.client)
+                    else:
+                        logging.debug(f"{self.client[0]}: Sending datagram")
+                        self.socket.sendto(b"DAT" + dat_to_bytes(data), self.client)
+
+                elif ans[:3] == b"DAT":
+                    data.update(bytes_to_dat(ans[3:]))
+
+                # ----------------------------------------IP list sync-------------------------------------
+                elif ans[:3] == b"RQS":
+                    logging.debug(f"{self.client[0]}: Sending Session List")
+                    sess = [i.client[0] if i.client else None for i in sessions]
+                    sess = set(sess)
+                    sess = sess - {None}
+                    sess = list(sess)
+                    sess.remove(self.client[0])
+                    self.socket.sendto(b"SES" + dat_to_bytes(sess), self.client)
+
+                elif ans[:3] == b"SES":
+                    sess = [i.client[0] if i.client else None for i in sessions]
+                    sess = set(sess)
+                    sess = sess - {None}
+                    logging.debug(
+                        f"{self.client[0]}: My sessions: {sess} Recieved: {set(bytes_to_dat(ans[3:]))} New: {list(set(bytes_to_dat(ans[3:]))-sess)}"
+                    )
+                    uncon = list(set(bytes_to_dat(ans[3:])) - sess)
+                    if not uncon:
+                        continue
+                    adr = random.choice(uncon)
+                    s = Session()
+                    sessions.append(s)
+                    self.socket.sendto(
+                        b"HOP"
+                        + socket.inet_aton(adr)
+                        + b"CON"
+                        + s.public_ip.encode("cp866")
+                        + b":"
+                        + str(s.public_port).encode("cp866"),
+                        self.client,
+                    )
+                # ----------------------------------------HOP Tracking-------------------------------------
+                elif ans[:3] == b"HOP":
+                    sess = [i.client[0] if i.client else None for i in sessions]
+
+                    ip = socket.inet_ntoa(ans[3:7])
+                    if ip in sess:
+                        s = sessions[sess.index(ip)]
+                        s.socket.sendto(ans[7:], s.client)
+
+                elif ans[:3] == b"CON":
+                    s = Session()
+                    adr, prt = ans[3:].decode("cp866").split(":")
+                    self.socket.sendto(
+                        b"HOP"
+                        + socket.inet_aton(adr)
+                        + b"RDY"
+                        + prt.encode("cp866")
+                        + b":"
+                        + s.public_ip.encode("cp866")
+                        + b":"
+                        + str(s.public_port).encode("cp866"),
+                        self.client,
+                    )
+                    try:
+                        s.make_connection(adr, int(prt))
+                        s.backlife_cycle(1)
+                        sessions.append(s)
+                    except:
+                        logging.error(f"{self.client[0]}: Connect initiation timeout!")
+
+                elif ans[:3] == b"RDY":
+                    myprt, adr, prt = ans[3:].decode("cp866").split(":")
+                    sess = [i.public_port for i in sessions]
+
+                    if int(myprt) in sess:
+                        s = sessions[sess.index(int(myprt))]
+                        try:
+                            s.make_connection(adr, int(prt))
+                            s.backlife_cycle(1)
+                        except:
+                            logging.error(
+                                f"{self.client[0]}: Connect stabilization timeout!"
+                            )
+                            sessions.remove(s)
+
+                # ----------------------------------------Disabled Trash-----------------------------------
+                elif ans[:3] == b"TRK":
+                    adr, prt = ans[3:].decode("cp866").split(",")
+                    sess = [i.client[0] for i in sessions]
+                    sess.remove(self.client[0])
+                    s = sessions[sess.index(adr)]
+
+                    s.socket.sendto(
+                        b"CN0"
+                        + f"{self.client[0].encode('cp866')}:{prt.encode('cp866')}",
+                        s.client,
+                    )
+
+                elif ans[:3] == b"CN0":
+                    s = Session()
+                    self.socket.sendto(
+                        b"CN1"
+                        + f"{s.public_ip.encode('cp866')}:{str(s.public_port).encode('cp866')}",
+                        self.client,
+                    )
+                    adr, prt = ans[3:].decode("cp866").split(":")
+                    s.make_connection(adr, int(prt))
+                    sessions.append(s)
+                elif ans[:3] == b"TRK":
+                    pass
+                else:
+                    logging.warning(f"{self.client[0]}: Malformed! !!!{ans}!!!")
---
+++ main.py
@@ -0,0 +1,150 @@
+from tkinter import *
+import socket
+import logging
+from datetime import datetime
+
+from app.classes import Protocol
+
+def addr2int(ip, port: int):
+    binport = bin(port)[2:].rjust(16, "0")
+    binip = "".join([bin(int(i))[2:].rjust(8, "0") for i in ip.split(".")])
+    return int(binip + binport, 2)
+
+def int2addr(num):
+    num = bin(num)[2:].rjust(48, "0")
+    print(num)
+    num = [
+        str(int(i, 2))
+        for i in [num[0:8], num[8:16], num[16:24], num[24:32], num[32:48]]
+    ]
+    return ".".join(num[0:4]), int(num[4])
+
+def tree2str(tree, indent_width=4):
+    def _tree2str(start, parent, tree, grandpa=None, indent=""):
+        outstr = ""
+        if parent != start:
+            if grandpa is None:  # Ask grandpa kids!
+                outstr += parent
+            else:
+                outstr += parent + "\n"
+        if parent not in tree:
+            return outstr
+        for child in tree[parent][:-1]:
+            outstr += indent + "├" + "─" * indent_width
+            outstr += _tree2str(start, child, tree, parent, indent + "│" + " " * 4)
+        
+        child = tree[parent][-1]
+        outstr += indent + "└" + "─" * indent_width
+        outstr += _tree2str(start, child, tree, parent, indent + " " * 5) + "\n"  # 4 -> 5
+        
+        return outstr
+    
+    outstr = "00000000: Root"
+    parent = outstr
+    outstr += "\n" + _tree2str(outstr, parent, tree)        
+    return outstr
+
+def loop():
+    updateForm()
+    root.after(500, loop)
+
+def updateForm():
+    if s == 0 :
+        lblInfo["text"] = f"Нет соединения с интернетом."
+    else :    
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+
+        lblInfo["text"] = f"Локальный IP: {socket.gethostbyname(socket.gethostname())} Внешний IP: {s.getPublic_ip()} \n Соединений: {len(Protocol.getSessions())} Время: {current_time}"
+        inputYourNumber.delete(0, END)
+        inputYourNumber.insert(0, f"{addr2int(s.public_ip,int(s.public_port))}")
+    lblChat["text"] = tree2str(Protocol.get_tree())
+    
+def btn_NewNumber():
+    global s
+    port = Protocol.randomport()
+    s = 0
+    try:
+        s = Protocol.Session(port)    
+    except Exception as e:
+        s = 0
+    updateForm()
+
+def btn_Connect():
+    if inputPeerNumber.get() != "" :
+        i, p = int2addr(int(inputPeerNumber.get()))
+        s.make_connection(i, p)
+        Protocol.sessions.append(s)
+        s.backlife_cycle(1)
+        updateForm()
+
+def btn_Message():
+    if inputMessage.get() != "" :
+        Protocol.data_add(inputMessage.get()) 
+        updateForm()
+
+def root_onClosing():
+    Protocol.data_dump()
+    root.destroy()
+
+def _onKeyRelease(event):
+    ctrl  = (event.state & 0x4) != 0
+    if event.keycode==88 and  ctrl and event.keysym.lower() != "x":
+        event.widget.event_generate("<<Cut>>")
+    if event.keycode==86 and  ctrl and event.keysym.lower() != "v":
+        event.widget.event_generate("<<Paste>>")
+    if event.keycode==67 and  ctrl and event.keysym.lower() != "c":
+        event.widget.event_generate("<<Copy>>")
+
+logging.basicConfig(format="%(asctime)s %(message)s", level=logging.DEBUG, filename="main.log")
+logging.info("Hello world!")
+logging.getLogger().setLevel(logging.DEBUG)
+logging.warning(f"Logger for DEBUG is running!")
+
+Protocol.data_load()
+
+root = Tk()
+root.title("Connector")
+root.bind_all("<Key>", _onKeyRelease, "+")
+root.protocol("WM_DELETE_WINDOW", root_onClosing)
+root.after_idle(loop)
+
+lblInfo = Label(root)
+lblInfo.place(relwidth=1, relheight=0.1)
+
+frameTop = Frame(root)
+frameTop.place(rely=0.1 ,relwidth=1, relheight=0.2) 
+
+lblYourNumber = Label(frameTop, text='Твой номер:')
+lblYourNumber.place(relwidth=0.4, relheight=0.5)
+
+inputYourNumber = Entry(frameTop)
+inputYourNumber.place(relx=0.4, relwidth=0.4, relheight=0.5)
+
+btnNewNumber = Button(frameTop, text='*', command=btn_NewNumber)
+btnNewNumber.place(relx=0.8, relwidth=0.2, relheight=0.5)
+
+inputPeerNumber = Entry(frameTop)
+inputPeerNumber.place(rely=0.5, relwidth=0.8, relheight=0.5)
+
+btnConnect = Button(frameTop, text='Con', command=btn_Connect)
+btnConnect.place(rely=0.5, relx=0.8, relwidth=0.2, relheight=0.5)
+
+frameCentr = Frame(root)
+frameCentr.place(rely=0.3, relwidth=1, relheight=0.6)
+
+lblChat = Label(frameCentr, bg='red', anchor="nw", justify="left", font=("Courier New", 10))
+lblChat.place(relheight=1, relwidth=1)
+
+frameBottom = Frame(root)
+frameBottom.place(rely=0.9, relwidth=1, relheight=0.1)
+
+inputMessage = Entry(frameBottom)
+inputMessage.place(relwidth=0.8, relheight=1)
+
+btnMessage = Button(frameBottom, text='Send', command=btn_Message)
+btnMessage.place(relx=0.8, relwidth=0.2, relheight=1)
+
+btn_NewNumber()
+
+root.mainloop()
